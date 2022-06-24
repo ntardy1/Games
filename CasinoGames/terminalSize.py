4f427b2aa1f75b501b67f862faa4e7469ede2369
ntardy1 <95262466+ntardy1@users.noreply.github.com>
2022-06-24 09:27:15 -0400

    Create terminalSize.py
    A script that, combined with user input, gets the dimensions of the current command prompt window, measured in character counts.

---
+++ CasinoGames/terminalSize.py
@@ -0,0 +1,50 @@
+
+# Importing Libraries
+import os
+from time import sleep
+import keyboard
+
+# Clearing the command prompt
+os.system('cls')
+
+# Initial variables
+delay = 1
+duration = 2
+list = []
+proceed = True
+
+# Getting the width of the command prompt
+while proceed:
+    list.append("-")
+    str = "".join(list)
+    print(f"\r{str}", end='')
+    for elem in range(duration * 10):
+        if (keyboard.is_pressed('esc')):
+            proceed = False
+        sleep(delay/(duration * 10))
+
+# Setting the width of the command prompt based on user input
+commandPromptWidth = len(list) - 1
+
+# Resetting variables
+os.system('cls')
+proceed = True
+str = ""
+list = []
+
+# Getting the length of the command prompt
+while proceed:
+    list.append("-\n")
+    str = "".join(list)
+    os.system('cls')
+    print(f"\r{str}", end="")
+    for elem in range(duration * 10):
+        if (keyboard.is_pressed('esc')):
+            proceed = False
+        sleep(delay/(duration * 10))
+
+# Setting the length of the command prompt based on user input
+commandPromptLength = len(list) - 1
+
+# Printing information
+print(f"Command Prompt Width: {commandPromptWidth}\nCommand Prompt Length: {commandPromptLength}")
