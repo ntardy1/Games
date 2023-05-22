7c25d7df2b407c35c27c621586b625a04ce30b4d
ntardy1 <tardy.nate@gmail.com>
2023-05-22 15:34:32 -0400

    Algorithm complete
    The algorithm can solve easier puzzles, but still falls short of being able to solve all puzzles. It would need to be added onto and made more robust in order to do so.

--- sudoSolve.cpp
+++ sudoSolve.cpp
@@ -2,31 +2,16 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdlib.h>
 
 bool any(int target, std::vector<int> vector){
     for (int i = 0; i < vector.size(); i++){
-        if (i == target){
+        if (vector[i] == target){
             return true;
         }
     }
     return false;
 }
-
-// Determining if target isn't in the vector
-bool allNot(int target, std::vector<int> vector){
-    int count;
-    count = 0;
-    for (int i = 0; i < vector.size(); i++){
-        if (i != target){
-            count++;
-        }
-    }
-    if (count == vector.size()){
-        return true;
-    }
-    return false;
-}
-
 void printResults(std::vector<std::vector<int>> masterVector){
     for (int i = 0; i < masterVector.size(); i++){
         for (int j = 0; j < masterVector[i].size(); j++){
@@ -49,7 +34,7 @@ int getIndex(int target, std::vector<int> vector){
 }
 
 int main(){
-    /*
+    std::system("cls");
     std::string first;
     std::string second;
     std::string third;
@@ -100,18 +85,6 @@ int main(){
         masterVector[7].push_back(eighth[i]);
         masterVector[8].push_back(ninth[i]);
     }
-    */
-    std::vector<std::vector<int>> masterVector;
-    masterVector = {{6,2,4,8,3,0,1,0,7},
-                    {0,1,8,9,2,0,3,6,5},
-                    {0,0,0,7,6,0,0,4,0},
-                    {0,0,0,0,8,0,9,0,1},
-                    {0,0,9,0,0,6,0,0,0},
-                    {1,4,0,0,0,0,5,8,0},
-                    {9,6,0,0,0,8,0,0,3},
-                    {0,0,0,0,1,0,0,5,0},
-                    {0,7,1,0,4,3,0,0,0}};
-
     int currentNumber;
 
     // Checking to make sure that the entries are valid
@@ -122,7 +95,7 @@ int main(){
                 if (currentNumber == 0){
                     NULL;
                 }
-                else if (currentNumber == masterVector[i][k] and j != k){
+                else if (currentNumber == masterVector[i][k] && j != k){
                     std::cout << "Error in Row " << i + 1 << "\n";
                     break;
                 }
@@ -135,69 +108,63 @@ int main(){
     int row;
     int column;
     int currentSpot;
-    int rowChecker;
     std::vector<int> temp1;
     std::vector<int> temp2;
     std::vector<int> candidates;
-    candidates = {0,1,2,3,4,5,6,7,8,9};
     row = 0;
     column = 0;
     counter = 0;
     index = 0;
-    rowChecker = 0;
 
     while (counter < 9){
-        candidates = {0,1,2,3,4,5,6,7,8,9};
-        if allNot(0, masterVector[row]){
-          counter++;
+        std::cout << "(" << row << ", " << column << ") \n";
+        candidates = {1,2,3,4,5,6,7,8,9};
+        if (row == 9){
+            break;
         }
-        // For loop (and conditional) to check if the row has been completely solved
-        for (int i = 0; i < masterVector.size(); i++){
-            if (masterVector[row][i] != 0){
-                rowChecker++;
+        if (!any(0, masterVector[row])){
+            if (row == 8){
+                counter++;
+            } else {
+                counter++;
+                row++;
             }
-        }
-        if (rowChecker == 9 & row == 8){
-            counter++;
-        } else if (rowChecker == 9){
-            row++;
         } else {
             currentSpot = masterVector[row][column];
             // If at the right edge of the board and the spot is filled, reset column and increase row
-            if (currentSpot != 0 and column == 8){
+            if (currentSpot != 0 && column == 8){
                 column = 0;
                 row++;
             // If at a filled spot that is not the right edge, increase column
-            } else if (currentSpot != 0 and column != 8){
+            } else if (currentSpot != 0 && column != 8){
                 column++;
             } else {
                 // Removing candidates based on the column
                 for (int i = 0; i < masterVector.size(); i++){
-                    if (masterVector[i][column] != 0){
-                        candidates.erase(candidates.begin() + getIndex(currentSpot, candidates));
+                    if (masterVector[i][column] != 0 && any(masterVector[i][column], candidates)){
+                        candidates.erase(candidates.begin() + getIndex(masterVector[i][column], candidates));
                     }
                 }
                 // Removing candidates based on the row
                 for (int i = 0; i < masterVector.size(); i++){
-                    if (masterVector[row][i] != 0){
-                        candidates.erase(candidates.begin() + getIndex(currentSpot, candidates));
+                    if (masterVector[row][i] != 0 && any(masterVector[row][i], candidates)){
+                        candidates.erase(candidates.begin() + getIndex(masterVector[row][i], candidates));
                     }
                 }
-                /*
                 // Removing candidates based on local groups
-                if (column == 0 or column == 3 or column == 6){
-                    if (row == 0 or row == 3 or row == 6){
+                if (column == 0 || column == 3 || column == 6){
+                    if (row == 0 || row == 3 || row == 6){
                         for (int i = 0; i < 3; i++){
                             for (int j = 0; j < 3; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                 candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
                         }
-                    } else if (row == 1 or row == 4 or row == 7){
+                    } else if (row == 1 || row == 4 || row == 7){
                         for (int i = -1; i < 2; i++){
                             for (int j = 0; j < 3; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                     candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
@@ -205,25 +172,25 @@ int main(){
                     } else {
                         for (int i = -2; i < 1; i++){
                             for (int j = 0; j < 3; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                     candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
                         }
                     }
-                } else if (column == 1 or column == 4 or column == 7){
-                    if (row == 0 or row == 3 or row == 6){
+                } else if (column == 1 || column == 4 || column == 7){
+                    if (row == 0 || row == 3 || row == 6){
                         for (int i = 0; i < 3; i++){
                             for (int j = -1; j < 2; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                     candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
                         }
-                    } else if (row == 1 or row == 4 or row == 7){
+                    } else if (row == 1 || row == 4 || row == 7){
                         for (int i = -1; i < 2; i++){
                             for (int j = -1; j < 2; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                     candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
@@ -231,25 +198,25 @@ int main(){
                     } else {
                         for (int i = -2; i < 1; i++){
                             for (int j = -1; j < 2; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                     candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
                         }
                     }
                 } else {
-                    if (row == 0 or row == 3 or row == 6){
+                    if (row == 0 || row == 3 || row == 6){
                         for (int i = 0; i < 3; i++){
                             for (int j = -2; j < 1; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                     candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
                         }
-                    } else if (row == 1 or row == 4 or row == 7){
+                    } else if (row == 1 || row == 4 || row == 7){
                         for (int i = -1; i < 2; i++){
                             for (int j = -1; j < 2; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                     candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
@@ -257,19 +224,32 @@ int main(){
                     } else {
                         for (int i = -2; i < 1; i++){
                             for (int j = -2; j < 1; j++){
-                                if (masterVector[row + i][column + j] != 0){
+                                if (masterVector[row + i][column + j] != 0 && any(masterVector[row + i][column + j], candidates)){
                                     candidates.erase(candidates.begin() + getIndex(masterVector[row + i][column + j], candidates));
                                 }
                             }
                         }
                     }
                 }
-                */
+                for (int i = 0; i < candidates.size(); i++){
+                    if (i == 0 && candidates.size() == 1){
+                        std::cout << "Candidates: {" << candidates[i] << "} \n";
+                    } else if (i == 0){
+                        std::cout << "Candidates: {" << candidates[i] << ", ";
+                    } else if (i == candidates.size() - 1){
+                        std::cout << candidates[i] << "} \n";
+                    } else {
+                        std::cout << candidates[i] << ", ";
+                    }
+                }
                 // If there is only one candidate left, it has to go in the current spot
                 if (candidates.size() == 1){
+                    std::cout << "Board changed \n";
                     masterVector[row][column] = candidates[0];
-                }
-                if (column == 8){
+                    row = 0;
+                    column = 0;
+                    counter = 0;
+                } else if (column == 8){
                     column = 0;
                     row++;
                 } else {
@@ -278,5 +258,12 @@ int main(){
             }
         }
     }
-    printResults(masterVector);
+    if (row == 9){
+        std::cout << "Board incompletely solved: \n";
+        std::cout << "\n";
+        printResults(masterVector);
+    } else {
+        std::cout << "Puzzle solved: \n";
+        printResults(masterVector);
+    }
 }
