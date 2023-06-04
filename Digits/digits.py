52aed3bb6cda45018a3f421c66a1ddda987faab4
ntardy1 <tardy.nate@gmail.com>
2023-06-04 13:15:37 -0400

    Update digits.py
    Something happened to digits.py during the development of sudoSolve.cpp. Problem resolved.

--- Digits/digits.py
+++ Digits/digits.py
@@ -8,23 +8,14 @@ Order of Operations
 import os
 
 # Variables
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
 numbers = list(range(6))
 operations = []
 result = 0 # the current result of all operations
-=======
->>>>>>> Stashed changes
 firstTensor = []
 tempFirstTensor = []
 operations = list(range(4))
 result = False
-<<<<<<< Updated upstream
-=======
 newGivens = []
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 # Functions
     
@@ -93,91 +84,28 @@ target = int(input('Please enter the target number: '))
 givens = input('Please enter the given numbers: ').split()
 givens = [int(num) for num in givens]
 
-<<<<<<< Updated upstream
-# Creating the first tensor
-columns = len(givens)
-for num in givens:
-    firstTensor.append([0]*(len(operations)+1))
-    columns -= 1
-    for operation in operations:
-        firstTensor[givens.index(num)][operations.index(operation)] = [0]*columns
-
-# Filling the first tensor
-rows = operations
-columns = len(givens)
-for matrix in firstTensor:
-    for row in rows:
-        if (row == 0): # Adding a row of the other given numbers for future reference
-            matrix[row] = [num for num in givens]
-            matrix[row].remove(givens[firstTensor.index(matrix)])
-        if (row == 1): # Addition
-            matrix[row] = [givens[firstTensor.index(matrix)] + givens[column] for column in range(columns)]
-            matrix[row].remove(givens[firstTensor.index(matrix)] + givens[firstTensor.index(matrix)])
-        elif (row == 2): # Subtraction
-            matrix[row] = [givens[firstTensor.index(matrix)] - givens[column] if givens[firstTensor.index(matrix)] - givens[column] >= 0 else 0 for column in range(columns)]
-            matrix[row].remove(0)
-        elif (row == 3): # Multiplication
-            matrix[row] = [givens[firstTensor.index(matrix)] * givens[column] for column in range(columns)]
-            matrix[row].remove(givens[firstTensor.index(matrix)] * givens[firstTensor.index(matrix)])
-        elif (row == 4): # Division
-            matrix[row] = [givens[firstTensor.index(matrix)] / givens[column] if givens[firstTensor.index(matrix)] % givens[column] == 0 else 0 for column in range(columns)]
-            matrix[row].remove(1)
-=======
-<<<<<<< Updated upstream
-    # Checking multiplication first
-    if (randomNumber == 0):
-        # Checking possibility of multiple elements' multiplication
-        for num1 in tempNumbers:
-            if (num1 == 1):
-                continue
-            else:
-                index_num1 = tempNumbers.index(num1)
-                tempNumbers.remove(num1)
-                for num2 in tempNumbers:
-                    index_num2 = tempNumbers.index(num2)
-                    tempMultiplication = num1 * num2
-                    tempNumbers[index_num2] = tempMultiplication
-                    # Checking division possibility
-                    result, tempNumbers, removal = checkDivision(result, tempNumbers)
-                    if (result == 0):
-                        break
-                    resetTempNumbers(tempNumbers, index_num2, num2, removal)
-                    # Checking subtraction possibility
-                    result, tempNumbers, removal = checkSubtraction(result, tempNumbers)
-                    if (result == 0):
-                        break
-                    resetTempNumbers(tempNumbers, index_num2, num2, removal)
-                if (result == 0):
-                    break
-                tempNumbers.insert(index_num1, num1)
-        if (result == 0):
-=======
 # Creating the first tensor
 firstTensor = createTensor(givens, operations, [])
 
 # Filling the first tensor
 firstTensor = fillTensor(firstTensor, givens, operations)
->>>>>>> Stashed changes
 
 tempFirstTensor = list(firstTensor) # Creating a tensor that will be iteratively modified
 tempGivens_1 = list(givens) # Creating a list of the givens that will be iteratively modified
 
-<<<<<<< Updated upstream
 # Testing all first-step operations
 columns = len(givens)
 for matrix_1 in firstTensor: # Iterating through to get the first number (that is the result of a previous operation)
     columns -= 1
-=======
+
 # Obtaining (and testing) a first-step operation
 for matrix_1 in firstTensor: # Iterating through to get the first number (that is the result of a previous operation)
     columns_1 = len(givens) - 1
->>>>>>> Stashed changes
     tempGivens_1 = list(givens)
     tempFirstTensor = list(firstTensor)
     tempFirstTensor.remove(matrix_1) # Removing the current matrix
     tempGivens_1.remove(tempGivens_1[firstTensor.index(matrix_1)]) # Removing the first number used from tempGivens_1
     for operation in operations: # Going through each row of the current matrix_1
-<<<<<<< Updated upstream
         for column in range(columns): # Going through each column of the current matrix_1
             firstNumberUsed = givens[firstTensor.index(matrix_1)] # Keeping track of the first number used for the first operation
             secondNumberUsed = matrix_1[0][column] # Keeping track of the second number used for the first operation
@@ -185,7 +113,7 @@ for matrix_1 in firstTensor: # Iterating through to get the first number (that i
             firstNumber = matrix_1[operation][column] # +1 because the first row is just the givens
             result = check(tempGivens_1, firstNumber, target)
             if (result):
-=======
+                break
         for column in list(range(columns_1)): # Going through each column of the current matrix_1
             newGivens = [] # Resetting the newGivens list
             firstNumberUsed = givens[firstTensor.index(matrix_1)] # Keeping track of the first number used for the first operation
@@ -265,35 +193,6 @@ for matrix_1 in firstTensor: # Iterating through to get the first number (that i
                         tempGivens_2.insert(tempFirstTensor.index(matrix_3), secondNumberUsed_1)
                         tempSecondTensor.insert(tempGivens_2.index(secondNumberUsed_1), matrix_4)
             newGivens.remove(firstNumber)
-            tempGivens_1.insert(column, secondNumberUsed) # Replacing the second number used
-            tempFirstTensor.insert(tempGivens_1.index(secondNumberUsed), matrix_2) # Replacing the matrix corresponding to the second number used
-        if (result):
->>>>>>> Stashed changes
-            break
-        # Checking the possibility of multiple elements' addition
-        for num1 in tempNumbers:
-            index_num1 = tempNumbers.index(num1)
-            tempNumbers.remove(num1)
-            for num2 in tempNumbers:
-                index_num2 = tempNumbers.index(num2)
-                tempAddition = num1 + num2
-                tempNumbers[index_num2] = tempAddition
-                # Checking division possibility
-                result, tempNumbers, removal = checkDivision(result, tempNumbers)
-                if (result == 0):
-                        break
-                resetTempNumbers(tempNumbers, index_num2, num2, removal)
-                # Checking subtraction possibility
-                result, tempNumbers, removal = checkSubtraction(result, tempNumbers)
-                if (result == 0):
-                        break
-                resetTempNumbers(tempNumbers, index_num2, num2, removal)
-            if (result == 0):
->>>>>>> Stashed changes
-                break
-            for matrix_2 in tempFirstTensor: # Removing the matrix that corresponds to the second number used
-                tempFirstTensor.remove(matrix_2) if all(secondNumberUsed != num for num in matrix_2[0]) else None
-                break
             tempGivens_1.insert(column, secondNumberUsed) # Replacing the second number used
             tempFirstTensor.insert(tempGivens_1.index(secondNumberUsed), matrix_2) # Replacing the matrix corresponding to the second number used
         if (result):
